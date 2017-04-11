3f84f62cd80a9d038917b6d6d1722485215ff0ed
Ivan Zinovyev <vanyazin@gmail.com>
2017-04-11 15:32:20 +0300

    Linear search module

--- lib/ruborithms/algorithms/linear_search.rb
+++ lib/ruborithms/algorithms/linear_search.rb
@@ -2,6 +2,12 @@ module Ruborithms
   module Algorithms
     module LinearSearch
       class << self
+        def included(mod)
+          mod.extend(ClassMethods)
+        end
+      end
+
+      module ClassMethods
         def linear_search(object, value)
           object.each_with_index do |item, index|
             return index if value == item
